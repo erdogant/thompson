bd157fa4fdaa7e29a30608a5f1e161aa9a29d850
erdogant <erdogant@gmail.com>
2022-03-11 18:41:04 +0100

    update url

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
      description="The multi-armed bandit by Thompson Sampling, UCB-Upper confidence Bound, and randomized sampling.",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/erdogant/thompson",
+     url="https://erdogant.github.io/thompson",
 	 download_url = 'https://github.com/erdogant/thompson/archive/'+new_version+'.tar.gz',
      packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
      include_package_data=True, # Must be true to include files depicted in MANIFEST.in
