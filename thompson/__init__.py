f54b1e15a86047e9ae50b7bd4611c08892ee7abb
erdogant <erdogant@gmail.com>
2025-04-25 12:25:55 +0200

    1.1.0

--- thompson/__init__.py
+++ thompson/__init__.py
@@ -11,7 +11,7 @@ from thompson.thompson import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 # Setup root logger
 _logger = logging.getLogger('thompson')
